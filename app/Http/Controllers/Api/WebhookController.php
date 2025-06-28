7f6d839206ddfd04ca8956bb98d4349e2c0364ac
Aizencode <aizen.code@gmail.com>
2025-06-28 01:36:46 -0500

    webhooks actualizacion

--- app/Http/Controllers/Api/WebhookController.php
+++ app/Http/Controllers/Api/WebhookController.php
@@ -10,6 +10,14 @@ class WebhookController extends Controller
 
     public function github(Request $request)
     {
+        $payload = $request->getContent();
+
+        $signature = 'sha1=' . hash_hmac('sha1', $payload, 'blueboxsubir');
+
+        if ($request->header('X-Hub-Signature') !== $signature) {
+            return response()->json(['status' => 'error', 'message' => 'Invalid signature'], 403);
+        }
+
         putenv('HOME=/home/blueboxdev');
         putenv('PATH=/usr/local/bin:/usr/bin:/bin');
         shell_exec('dploy deploy master');
